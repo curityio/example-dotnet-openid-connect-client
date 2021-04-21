fcd08ee5ea9ae0c0bcc2d69483987b6838a63f52
Gary Archer <garyarcher36@gmail.com>
2021-04-21 11:35:59 +0100

    Fixed default Curity URL

---
+++ Backup/example-dotnet-openid-connect-client/Controllers/CallbackController.cs
@@ -0,0 +1,170 @@
+﻿/*
+ * Copyright (C) 2017 Curity AB.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using static System.Convert;
+
+namespace exampledotnetopenidconnectclient.Controllers
+{
+    public class CallbackController : Controller
+    {
+        private static Helpers.Client _client = Helpers.Client.Instance;
+        private string issuer = App_Start.AppConfig.Instance.GetIssuer();
+        private string jwks_uri = App_Start.AppConfig.Instance.GetJwksUri();
+
+        private JObject id_token_obj;
+
+        static readonly HttpClient client = new HttpClient();
+
+        public ActionResult Index()
+        {
+            try
+            {
+                string responseString = _client.GetToken(Request.QueryString["code"]);
+                SaveDataToSession(responseString);
+            }
+            catch (JwtValidationException e)
+            {
+                Session["error"] = e.Message;
+            }
+            catch (OAuthClientException e)
+            {
+                Session["error"] = e.Message;
+            }
+
+            return Redirect("/");
+        }
+
+        public void SaveDataToSession(String curityResponse)
+        {
+            JObject jsonObj = JObject.Parse(curityResponse);
+
+            Session["access_token"] = jsonObj.GetValue("access_token");
+            Session["refresh_token"] = jsonObj.GetValue("refresh_token");
+            Session["scope"] = jsonObj.GetValue("scope");
+
+            if (jsonObj.GetValue("id_token") != null && IsJwtValid(jsonObj.GetValue("id_token").ToString()))
+            {
+                Session["id_token"] = jsonObj.GetValue("id_token");
+                Session["id_token_json0"] = id_token_obj.GetValue("decoded_header").ToString();
+                Session["id_token_json1"] = id_token_obj.GetValue("decoded_payload").ToString();
+            }
+        }
+
+
+        public String SafeDecodeBase64(String str)
+        {
+            return System.Text.Encoding.UTF8.GetString(
+                getPaddedBase64String(str));
+        }
+
+        private byte[] getPaddedBase64String(string base64Url)
+        {
+            string padded = base64Url.Length % 4 == 0 ? base64Url : base64Url + "====".Substring(base64Url.Length % 4);
+            string base64 = padded.Replace("_", "/").Replace("-", "+");
+            return FromBase64String(base64);
+        }
+
+        public bool IsJwtValid(String jwt)
+        {
+            string[] jwtParts = jwt.Split('.');
+
+            String decodedHeader = SafeDecodeBase64(jwtParts[0]);
+            String decodedPayload = SafeDecodeBase64(jwtParts[1]);
+            id_token_obj = new JObject
+            {
+                {"decoded_header", decodedHeader },
+                {"decoded_payload", decodedPayload }
+            };
+
+            String keyId = JObject.Parse(decodedHeader).GetValue("kid").ToString();
+            JToken keyFound = (JToken)System.Web.HttpContext.Current.Application[keyId];
+
+            if (keyFound == null)
+            {
+                keyFound = FetchKeys(keyId);
+                if (keyFound == null)
+                {
+                    throw new JwtValidationException("Key not found in JWKS endpoint or Application State");
+                }
+            }
+
+            if (!JObject.Parse(decodedPayload).GetValue("iss").ToString().Equals(issuer))
+            {
+                throw new JwtValidationException("Issuer not validated");
+            }
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(
+              new RSAParameters()
+              {
+                  Modulus = getPaddedBase64String(keyFound["n"].ToString()),
+                  Exponent = getPaddedBase64String(keyFound["e"].ToString())
+              });
+
+            SHA256 sha256 = SHA256.Create();
+            byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(jwtParts[0] + '.' + jwtParts[1]));
+
+            RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+            rsaDeformatter.SetHashAlgorithm("SHA256");
+            if (rsaDeformatter.VerifySignature(hash, getPaddedBase64String(jwtParts[2])))
+            {
+                return true; //Jwt Validated
+            }
+            else
+            {
+                throw new JwtValidationException("Could not validate signature of JWT");
+
+            }
+        }
+
+        public JToken FetchKeys(String keyId)
+        {
+            var jwksclient = new HttpClient();
+            jwksclient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = jwksclient.GetAsync(jwks_uri).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+                string responseString = responseContent.ReadAsStringAsync().Result;
+
+                foreach (JToken key in JObject.Parse(responseString).GetValue("keys").ToArray())
+                {
+                    if (key["kid"].ToString().Equals(keyId))
+                    {
+                        System.Web.HttpContext.Current.Application.Lock();
+                        System.Web.HttpContext.Current.Application[keyId] = key;
+                        System.Web.HttpContext.Current.Application.UnLock();
+
+                        return key;
+                    }
+                }
+
+                throw new JwtValidationException("Key not found in JWKS endpoint");
+            }
+
+            throw new JwtValidationException("Could not contact JWKS endpoint");
+        }
+    }
+}
---
+++ Backup/example-dotnet-openid-connect-client/Helpers/Client.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Copyright (C) 2017 Curity AB.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace exampledotnetopenidconnectclient.Helpers
+{
+    public class Client
+    {
+        private static Client instance;
+        private static App_Start.AppConfig _config = App_Start.AppConfig.Instance;
+
+        private string client_id;
+        private string client_secret;
+        private string redirect_uri;
+        private string token_endpoint;
+        private string issuer;
+        private string jwks_uri;
+        private string revocation_endpoint;
+        private string authorization_endpoint;
+        private string scope;
+
+        private Client()
+        {
+            client_id = _config.GetClientId();
+            client_secret = _config.GetClientSecret();
+            redirect_uri = _config.GetRedirectUri();
+            token_endpoint = _config.GetTokenEndpoint();
+            issuer = _config.GetIssuer();
+            jwks_uri = _config.GetJwksUri();
+            revocation_endpoint = _config.GetRevocationEndpoint();
+            authorization_endpoint = _config.GetAuthorizationEndpoint();
+            scope = _config.GetScope();
+        }
+
+        public void Revoke(String refresh_token)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "token", refresh_token },
+                { "client_secret", client_secret },
+                { "client_id" , client_id }
+            };
+
+            HttpClient revokeClient = new HttpClient();
+            var content = new FormUrlEncodedContent(values);
+            var response = revokeClient.PostAsync(revocation_endpoint, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+                string responseString = responseContent.ReadAsStringAsync().Result;
+
+                return;
+            }
+
+            throw new OAuthClientException("Could not revoke the refresh token");
+        }
+
+        public String Refresh(String refresh_token)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "grant_type", "refresh_token" },
+                { "refresh_token", refresh_token },
+                { "client_secret", client_secret },
+                { "client_id" , client_id }
+            };
+
+            HttpClient refreshClient = new HttpClient();
+            var content = new FormUrlEncodedContent(values);
+            var response = refreshClient.PostAsync(token_endpoint, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+
+                return responseContent.ReadAsStringAsync().Result;
+            }
+
+            throw new OAuthClientException("Could not refresh the tokens");
+        }
+
+        public String GetAuthnReqUrl()
+        {
+            return authorization_endpoint + "?client_id="
+               + client_id + "&response_type=code"
+               + "&scope=" + scope + "&redirect_uri=" + redirect_uri;
+        }
+
+        public String GetToken(String code)
+        {
+            var values = new Dictionary<string, string>
+            {
+                { "grant_type", "authorization_code" },
+                { "client_id", client_id},
+                { "client_secret", client_secret },
+                { "code" , code },
+                { "redirect_uri", redirect_uri}
+            };
+
+
+            HttpClient tokenClient = new HttpClient();
+            var content = new FormUrlEncodedContent(values);
+            var response = tokenClient.PostAsync(token_endpoint, content).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+
+                return responseContent.ReadAsStringAsync().Result;
+            }
+
+            throw new OAuthClientException("Token request failed with status code: " + response.StatusCode);
+        }
+
+        public static Client Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Client();
+                }
+                return instance;
+            }
+        }
+    }
+}
